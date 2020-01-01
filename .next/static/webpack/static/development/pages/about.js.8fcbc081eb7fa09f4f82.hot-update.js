6fd286564791d6fb29ff1f504ec25f7592853d04
rakeeb123 <rakeeb.hossain1@gmail.com>
2020-01-01 11:28:33 -0500

    Initial commit

---
+++ .next/static/webpack/static/development/pages/about.js.8fcbc081eb7fa09f4f82.hot-update.js
@@ -0,0 +1,39 @@
+webpackHotUpdate("static/development/pages/about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return About; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/rakeeb/Code/Web/DropShare/pages/about.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+function About() {
+  var content = __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "This is the about page"));
+
+  return typeof content;
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.8fcbc081eb7fa09f4f82.hot-update.js.map
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+	let content = (
+		<div>
+			<p>This is the about page</p>
+		</div>
+	)
+
+	return content
+}
